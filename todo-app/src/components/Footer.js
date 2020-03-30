9a6f22991a520c34e706abfd36ed0725b5bd1f35
at-phuongle <phuong.le@asiantech.vn>
2020-03-30 17:53:21 +0700

    update component footer

--- todo-app/src/components/Footer.js
+++ todo-app/src/components/Footer.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Footer(props) {
-let todos = props;
-  return (
-    <footer className="page-footer">
-      <ul className="footer-list">
-        <li className="footer-item">{todos.todo.length} iteams left</li>
-        <li className="footer-item"><a href="#">All task</a></li>
-        <li className="footer-item"><a href="#">Active</a></li>
-        <li className="footer-item"><a href="#">Completed</a></li>
-      </ul>
-    </footer>
-  );
+// function Footer(props) {
+class Footer extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      // let todos = this.props;
+      <footer className="page-footer">
+        <ul className="footer-list">
+          <li className="footer-item">{this.props.todo.length} iteams left</li>
+          <li className="footer-item"><a href="#">All task</a></li>
+          <li className="footer-item"><a href="#">Active</a></li>
+          <li className="footer-item"><a href="#">Completed</a></li>
+        </ul>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
